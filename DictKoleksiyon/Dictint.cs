ee2a006430c4af41bf7cfc6fd7d96ba6c6ffe306
Ramazan GÜRSOY <gursoy90@gmail.com>
2021-01-27 18:55:14 +0300

    Add project files.

---
+++ DictKoleksiyon/Dictint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictKoleksiyon
+{
+    class Dictint<TKey,TValue>
+    {
+        TKey[] anahtar;
+        TValue[] deger;
+        TKey[] tempanahtar;
+        TValue[] tempdeger;
+        public Dictint()
+        {
+            anahtar = new TKey[0];
+            deger = new TValue[0];
+
+        }
+
+        public void Add(TKey sayi,TValue yazi)
+        {
+            tempanahtar = anahtar;
+            tempdeger = deger;
+            anahtar = new TKey[anahtar.Length + 1];
+            deger = new TValue[deger.Length + 1];
+
+            for (int i = 0; i < tempanahtar.Length; i++)
+            {
+                anahtar[i] = tempanahtar[i];
+                deger[i] = tempdeger[i];
+            }
+            
+            anahtar[anahtar.Length - 1] = sayi;
+            deger[deger.Length - 1] = yazi;
+        }
+
+    }
+}
---
+++ DictKoleksiyon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictKoleksiyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> sozluk = new Dictionary<int, string>();
+
+            sozluk.Add(1990, "Ramazan");
+            sozluk.Add(1992, "Özge");
+            sozluk.Add(2020, "Aslan");
+            int dtarih = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(sozluk[dtarih]);
+
+            Dictint<int, string> sozluk1 = new Dictint<int, string>();
+            sozluk1.Add(1990, "Ramazan");
+
+            
+
+
+
+
+        }
+    }
+}
